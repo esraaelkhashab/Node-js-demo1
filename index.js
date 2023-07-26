18c56410cd54103c54ec831434171ecf9341b851
esraaelkhashab <esraaelkhashab66@gmail.com>
2023-07-26 08:44:00 +0000

    add Docker file

--- index.js
+++ index.js
@@ -1,53 +1,17 @@
+'use strict';
+
 const express = require('express');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
 const app = express();
-const request = require('request');
-const wikip = require('wiki-infobox-parser');
-
-//ejs
-app.set("view engine", 'ejs');
-
-//routes
-app.get('/', (req,res) =>{
-    res.render('index');
+app.get('/', (req, res) => {
+  res.send('Hello World');
 });
 
-app.get('/index', (req,response) =>{
-    let url = "https://en.wikipedia.org/w/api.php"
-    let params = {
-        action: "opensearch",
-        search: req.query.person,
-        limit: "1",
-        namespace: "0",
-        format: "json"
-    }
-
-    url = url + "?"
-    Object.keys(params).forEach( (key) => {
-        url += '&' + key + '=' + params[key]; 
-    });
-
-    //get wikip search string
-    request(url,(err,res, body) =>{
-        if(err) {
-            response.redirect('404');
-        }
-            result = JSON.parse(body);
-            x = result[3][0];
-            x = x.substring(30, x.length); 
-            //get wikip json
-            wikip(x , (err, final) => {
-                if (err){
-                    response.redirect('404');
-                }
-                else{
-                    const answers = final;
-                    response.send(answers);
-                }
-            });
-    });
-
-    
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
 });
-
-//port
-app.listen(3000, console.log("Listening at port 3000..."))
